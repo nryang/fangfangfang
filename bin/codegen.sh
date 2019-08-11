43adb97bf6a21eb869da12dd14f11ba97988a38c
Nathan Yang <nathanryang@gmail.com>
2019-08-11 11:07:05 -0700

    Update MANIFEST, don't include jar

--- bin/codegen.sh
+++ bin/codegen.sh
@@ -2,11 +2,14 @@
 #
 # Usage: bash codegen.sh
 #
-# Description: Generates Python Connexion server stubs from an OpenAPI document
+# Description: This downloads the openapi-generator JAR (if it does not already
+# exist) and generates Python Connexion server files from an OpenAPI document
 # using openapi-generator
 #
 # Requirements: Java 8+
 #
+
+wget -nc http://central.maven.org/maven2/org/openapitools/openapi-generator-cli/4.1.0/openapi-generator-cli-4.1.0.jar -O openapi-generator-cli.jar
 
 java -jar ./openapi-generator-cli.jar generate  \
 -i ../openapi/openapi.yaml \
