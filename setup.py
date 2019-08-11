4769f25a9cb7c329554fb84e21d65c233f749cd8
Nathan Yang <nathanryang@gmail.com>
2019-08-11 10:15:52 -0700

    Consistent quotes in setup.py, fix description

--- setup.py
+++ setup.py
@@ -19,26 +19,26 @@ except ImportError:
 
 
 def load_requirements(fname):
-    reqs = parse_requirements(fname, session="test")
+    reqs = parse_requirements(fname, session='test')
     return [str(ir.req) for ir in reqs]
 
 
 setup(
     name=NAME,
     version=VERSION,
-    description='OpenAPI Petstore',
-    author_email="nryang@users.noreply.github.com",
-    url="https://github.com/nryang/fangfangfang",
-    keywords=["defang", "refang"],
+    description='A web application that allows you to defang and refang URLs in text.',
+    author_email='nryang@users.noreply.github.com',
+    url='https://github.com/nryang/fangfangfang',
+    keywords=['defang', 'refang'],
     python_requires='>3.6',
     install_requires=load_requirements('requirements.txt'),
     packages=find_packages(),
     package_data={'': ['openapi/openapi.yaml']},
     include_package_data=True,
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
     entry_points={
         'console_scripts': ['fangfangfang=fangfangfang.__main__:main']},
